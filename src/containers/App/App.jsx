6fa5e8c2c8247c749e0848b4ad769065df81bf39
Kat Kolomycew <kat.kolomycew@Nology.local>
2019-11-26 15:55:31 +0000

    finished basic setup

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./App.module.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p className={styles.test}>test</p>
+      </div>
+    );
+  }
+}
+
+export default App;
