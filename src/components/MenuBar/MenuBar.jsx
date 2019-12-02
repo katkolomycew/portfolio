a868110ccb0be7f84b6112efd821cd027b31d8ea
Kat Kolomycew <kat.kolomycew@Nology.local>
2019-12-02 12:12:18 +0000

    added fontawesome and changed photo + styling

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/fontawesome-free-brands";
+import { faGithub } from "@fortawesome/fontawesome-free-brands";
 import styles from "./MenuBar.module.scss";
 
 class MenuBar extends Component {
@@ -6,12 +13,13 @@ class MenuBar extends Component {
     return (
       <section className={styles.barBackground}>
         <p className={styles.barLogo}>KK</p>
-        <p>Work</p>
-        <p>Blog</p>
-        <p>Contact</p>
+        <FontAwesomeIcon icon={faHome} class="fas fa-home" />
+        <FontAwesomeIcon icon={faBriefcase} class="fas fa-briefcase" />
+        <FontAwesomeIcon icon={faEdit} class="fas fa-edit" />
+        <FontAwesomeIcon icon={faPaperPlane} class="fas fa-paper-plane" />
         <article className={styles.barSocial}>
-          <p>L</p>
-          <p>G</p>
+          <FontAwesomeIcon icon={faLinkedin} class="fab fa-linkedin" />
+          <FontAwesomeIcon icon={faGithub} class="fab fa-github" />
         </article>
       </section>
     );
