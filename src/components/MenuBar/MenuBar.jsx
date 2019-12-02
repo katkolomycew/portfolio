e925a84da769468ef561424d492e1122d1575456
Kat Kolomycew <kat.kolomycew@Nology.local>
2019-12-02 12:59:19 +0000

    added cv icon

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin } from "@fortawesome/fontawesome-free-brands";
 import { faGithub } from "@fortawesome/fontawesome-free-brands";
@@ -16,6 +17,7 @@ class MenuBar extends Component {
         <FontAwesomeIcon icon={faHome} className={styles.homeIcon} />
         <FontAwesomeIcon icon={faBriefcase} className={styles.workIcon} />
         <FontAwesomeIcon icon={faEdit} className={styles.blogIcon} />
+        <FontAwesomeIcon icon={faFile} className={styles.cvIcon} />
         <FontAwesomeIcon icon={faPaperPlane} className={styles.contactIcon} />
         <article className={styles.barSocial}>
           <FontAwesomeIcon icon={faLinkedin} className={styles.linkedinIcon} />
