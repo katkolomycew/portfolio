c041956930bb68b7be27bbcd8136ceb31547f934
Kat Kolomycew <kat.kolomycew@Nology.local>
2019-12-04 17:00:27 +0000

    started work on menubar hover-text

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -10,24 +10,90 @@ import { faGithub } from "@fortawesome/fontawesome-free-brands";
 import styles from "./MenuBar.module.scss";
 
 class MenuBar extends Component {
+  state = {
+    showHomeText: false,
+    showWorkText: false,
+    showBlogText: false,
+    showCvText: false,
+    showContactText: false
+  };
+
+  showMenuText = () => {
+    this.setState({
+      showHomeText: !this.state.showHomeText,
+      showWorkText: !this.state.showWorkText,
+      showBlogText: !this.state.showBlogText,
+      showCvText: !this.state.showCvText,
+      showContactText: !this.state.showContactText
+    });
+  };
+
   render() {
     return (
       <section className={styles.barBackground}>
         <p className={styles.barLogo}>KK</p>
-        <a href="/">
-          <FontAwesomeIcon icon={faHome} className={styles.homeIcon} />
+        <a
+          href="/"
+          ref="home"
+          id={styles.home}
+          onMouseEnter={() => this.showMenuText()}
+          onMouseLeave={() => this.showMenuText()}
+        >
+          <div style={{ opacity: this.state.showHomeText ? 0 : 1 }}>
+            <FontAwesomeIcon icon={faHome} className={styles.homeIcon} />
+          </div>
+          <p style={{ opacity: this.state.showHomeText ? 1 : 0 }}>Home</p>
         </a>
-        <a href="/work">
-          <FontAwesomeIcon icon={faBriefcase} className={styles.workIcon} />
+        <a
+          href="/work"
+          ref="work"
+          id={styles.work}
+          onMouseEnter={() => this.showMenuText()}
+          onMouseLeave={() => this.showMenuText()}
+        >
+          <div style={{ opacity: this.state.showWorkText ? 0 : 1 }}>
+            <FontAwesomeIcon icon={faBriefcase} className={styles.workIcon} />
+          </div>
+          <p style={{ opacity: this.state.showWorkText ? 1 : 0 }}>Work</p>
         </a>
-        <a href="/blog">
-          <FontAwesomeIcon icon={faEdit} className={styles.blogIcon} />
+        <a
+          href="/blog"
+          ref="blog"
+          id={styles.blog}
+          onMouseEnter={() => this.showMenuText()}
+          onMouseLeave={() => this.showMenuText()}
+        >
+          <div style={{ opacity: this.state.showBlogText ? 0 : 1 }}>
+            <FontAwesomeIcon icon={faEdit} className={styles.blogIcon} />
+          </div>
+          <p style={{ opacity: this.state.showBlogText ? 1 : 0 }}>Blog</p>
         </a>
-        <a href="/cv">
-          <FontAwesomeIcon icon={faFile} className={styles.cvIcon} />
+        <a
+          href="/cv"
+          ref="cv"
+          id={styles.cv}
+          onMouseEnter={() => this.showMenuText()}
+          onMouseLeave={() => this.showMenuText()}
+        >
+          <div style={{ opacity: this.state.showCvText ? 0 : 1 }}>
+            <FontAwesomeIcon icon={faFile} className={styles.cvIcon} />
+          </div>
+          <p style={{ opacity: this.state.showCvText ? 1 : 0 }}>CV</p>
         </a>
-        <a href="/contact">
-          <FontAwesomeIcon icon={faPaperPlane} className={styles.contactIcon} />
+        <a
+          href="/contact"
+          ref="contact"
+          id={styles.contact}
+          onMouseEnter={() => this.showMenuText()}
+          onMouseLeave={() => this.showMenuText()}
+        >
+          <div style={{ opacity: this.state.showContactText ? 0 : 1 }}>
+            <FontAwesomeIcon
+              icon={faPaperPlane}
+              className={styles.contactIcon}
+            />
+          </div>
+          <p style={{ opacity: this.state.showContactText ? 1 : 0 }}>Contact</p>
         </a>
         <article className={styles.barSocial}>
           <a
